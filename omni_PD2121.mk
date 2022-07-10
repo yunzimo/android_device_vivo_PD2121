5be4615ee23adb995acfb7e6f662d287b9d36e37
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-10 11:16:09 +0200

    PD2121: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: vivo Model: V2121A Script version: 2.3.0

---
+++ omni_PD2121.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from PD2121 device
+$(call inherit-product, device/vivo/PD2121/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := PD2121
+PRODUCT_NAME := omni_PD2121
+PRODUCT_BRAND := vivo
+PRODUCT_MODEL := V2121A
+PRODUCT_MANUFACTURER := vivo
