3964f31dfc345c7eeeaf65733bdb934fdec46819
yunzimo <yunzimolikeyou@outlook.com>
2024-04-07 05:40:13 +0800

    Update omni_PD2121.mk

--- omni_PD2121.mk
+++ omni_PD2121.mk
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/vivo/PD2121/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
